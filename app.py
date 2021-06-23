e5ac82d61d08ccbe1006cc361499f63377b24726
Maxzkp <35385842+Maxzkp@users.noreply.github.com>
2021-06-23 15:31:56 -0500

    Test commit 3

--- app.py
+++ app.py
@@ -29,8 +29,9 @@ def mostrar_catalogo():
     
 @app.route('/juegos/filtros', methods = ['GET'])# Sirve para aplicar los filtros de la primera pagina
 def juegos_filtros():
-    print(request.get_data())
-    xml_info = xmltodict.parse(request.get_data())
+    request.get_data()
+    print(request.data)
+    xml_info = xmltodict.parse(request.data)
     #print(xml_info)
     query =''
     if xml_info["juego"]["titulo"] != None:
